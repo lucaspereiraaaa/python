cf515e71d935573e6235d316e930f5f1bb34b1fa
lukinhaaaaa <lucaspereirademorais123456789@gmail.com>
2024-08-21 20:46:49 -0300

    ex04.py
    add exercicio 04, feito com amor e carinho

---
+++ ex04.py
@@ -0,0 +1,13 @@
+# receba dois numeros ao usuario
+nm1 = float(input("escreva o primeiro digito: "))
+nm2 = float(input("escreva o segundo digito: "))
+
+# encontre o maior numero 
+if  nm1 > nm2:
+    maior = nm1
+
+else: 
+    maior = nm2
+
+#exiba o maior numero
+print(f"o maior numero e: {maior}") 
