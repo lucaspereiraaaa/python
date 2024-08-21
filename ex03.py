cbfee30a1a2fc053e69c264202b437e740d7f6f7
lukinhaaaaa <lucaspereirademorais123456789@gmail.com>
2024-08-21 20:27:40 -0300

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,7 @@
+numero1= float(input("escreva o primeiro digito")) 
+numero2= float(input("escreva o segundo digito"))
+numero3= float(input("escreva o terceiro digito"))
+
+soma = numero1 + numero2 + numero3
+
+print("a soma dos numeros e:", soma)
