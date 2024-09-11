5c9423b58eb0b6543f3e1f8dbe16e64052af0654
lukinhaaaaa <lucaspereirademorais123456789@gmail.com>
2024-09-10 21:00:56 -0300

    adicionei o ex05.py

---
+++ ex05.py
@@ -0,0 +1,8 @@
+idade = int(input("Digite a sua idade: "))
+
+# Verifica se a idade é maior ou menor de 18 anos
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print("Você é menor de idade.")
+
